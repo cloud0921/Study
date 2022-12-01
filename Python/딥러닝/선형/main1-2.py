038e4363840ab5d76bfab839ef959e09935727cf
cloud0921 <ssjjhhcc@gmail.com>
2022-12-01 17:34:26 +0900

    commit

---
+++ Python/딥러닝/선형/main1-2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+x = np.array([2, 4, 6 ,8])
+y = np.array([81, 93, 91, 97])
+
+#x의 평균값을 구합니다.
+mx = np.mean(x)
+
+#y의 평균값을 구합니다.
+my = np.mean(y)
+
+
+# 출력으로 확인합니다.
+print("x의 평균값:", mx)
+print("y의 평균값:", my)
+
